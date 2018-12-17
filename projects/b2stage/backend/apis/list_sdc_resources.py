6391b8b71d5fa5d24320f46ef5146df8bb7b28ca
merret <buurman@dkrz.de>
2018-12-17 17:44:13 +0100

    Added log messages to list-resources.

--- projects/b2stage/backend/apis/list_sdc_resources.py
+++ projects/b2stage/backend/apis/list_sdc_resources.py
@@ -2,6 +2,7 @@
 
 from b2stage.apis.commons.cluster import ClusterContainerEndpoint as Endpoint
 from utilities.logs import get_logger
+from b2stage.apis.commons.queue import log_start, log_submitted_async
 from restapi.flask_ext.flask_celery import CeleryExt
 
 log = get_logger(__name__)
@@ -11,8 +12,11 @@ class ListResources(Endpoint):
 
     def post(self):
 
-        imain = self.get_service_instance(service_name='irods')
+        taskname = 'list_sdc_resources'
         json_input = self.get_input()
+        log_start(self, taskname, json_input)
+
+        imain = self.get_service_instance(service_name='irods')
         task = CeleryExt.list_resources.apply_async(
             args=[
                 self.get_irods_batch_path(imain),
@@ -21,4 +25,5 @@ class ListResources(Endpoint):
             ]
         )
         log.warning("Async job: %s", task.id)
+        log_submitted_async(self, taskname, json_input, task.id)
         return self.return_async_id(task.id)
